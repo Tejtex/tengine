8fc442e8f8e1b093067628328194601c14908cf6
Tejtex <raxeli1.mc@gmail.com>
2025-05-03 09:37:33 +0200

    xd

--- src/tengine/core/system.py
+++ src/tengine/core/system.py
@@ -1,6 +1,6 @@
 from tengine.core.entity import Entity
 from tengine.core.world import WorldInfo
-
+#a
 class System:
     def update(self, entities: list[Entity], world_info: WorldInfo):
         pass
